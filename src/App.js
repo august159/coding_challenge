bc24fabed70862e1ff99b9ce9c52b10390e31a1f
august159 <augustin.leman@gmail.com>
2021-04-05 15:47:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import Temperature from "./components/Temperature";
+import CustomizeImage from "./components/CustomizeImage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/temperature">Temperature</Link>
+        <Link to="/customize-image">Customize Image</Link>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/temperature" exact component={Temperature} />
+        <Route path="/customize-image" exact component={CustomizeImage} />
+        {/* <Route render={() => <h1>404</h1>} /> */}
+      </Switch>
     </div>
   );
 }
